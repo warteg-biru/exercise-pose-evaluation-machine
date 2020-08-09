167e00731689b32773a7d500845e92c856875d44
Kevin Janada <kevin.janada@gmail.com>
2020-08-09 11:32:14 +0700

    terima input base_path dari cli, remove argument folder dari function resize

--- data_preproccess/resize.py
+++ data_preproccess/resize.py
@@ -1,25 +1,36 @@
 import os
+import sys
 
 # Define base path for the dataset
 base_path = "/home/kevin/projects/dataset-handsup-to-exercise"
 save_base_path = '/home/kevin/projects/dataset-handsup-to-exercise'
 
-def transform_video(base, folder, filename):
+# get base path argument from cli
+def get_base_path_argument():
+    try:
+        path_arg = sys.argv[1]
+        return path_arg
+    except:
+        return None
+
+def transform_video(base, filename):
     # Check if dir exists
     if not os.path.exists(save_base_path + '/preprocessed_videos'):
         os.mkdir(save_base_path + '/preprocessed_videos')
         print("Made preprocess directory!")
-    if not os.path.exists(save_base_path + '/preprocessed_videos/' + folder):
-        os.mkdir(save_base_path + '/preprocessed_videos/' + folder)
-        print("Made " + folder + " directory!")
 
     # Resize video to optimal size
-    file_path = base + '/' + folder + '/' + filename
-    save_path = save_base_path + '/preprocessed_videos/' + folder + '/' + filename + '.mp4'
+    file_path = base + '/' + filename
+    save_path = save_base_path + '/preprocessed_videos/' + filename + '.mp4'
     os.system("ffmpeg -i " + file_path + " -vf scale=720:404 -an " + save_path)
     print("Resized, muted and saved!")
 
 if __name__ == '__main__':
+    base_path_arg = get_base_path_argument()
+    if base_path_arg is not None:
+        base_path = base_path_arg
+        save_base_path = base_path_arg
+
     # Get dataset folders
     # dirs = os.listdir(base_path)
     files = os.listdir(base_path)
@@ -28,4 +39,4 @@ if __name__ == '__main__':
     # for folder in dirs:
     #     files = os.listdir(base_path + '/' + folder)
     for filename in files:
-        transform_video(base_path, "ASD", filename)
+        transform_video(base_path, filename)
