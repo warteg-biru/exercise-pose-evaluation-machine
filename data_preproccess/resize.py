1791eee4e908ad0b5c99a373b9b6f5483037b861
Agustinus Theodorus <agustinus.theodorus@binus.edu>
2020-10-08 19:52:47 +0700

    moved data preprocessing scripts to one folder

--- data_preproccess/resize.py
+++ data_preproccess/resize.py
@@ -1,36 +1,31 @@
 import os
-import sys
 
-# get base path argument from cli
-def get_base_path_argument():
-    try:
-        path_arg = sys.argv[1]
-        return path_arg
-    except:
-        return None
+# Define base path for the dataset
+base_path = "/home/kevin/Videos/exercise-raw-dataset/2020-08-21"
+save_base_path = '/home/kevin/Videos/exercise-raw-dataset/2020-08-21'
 
-def transform_video(base, filename, save_base_path):
+def transform_video(base, folder, filename):
     # Check if dir exists
     if not os.path.exists(save_base_path + '/preprocessed_videos'):
         os.mkdir(save_base_path + '/preprocessed_videos')
         print("Made preprocess directory!")
+    if not os.path.exists(save_base_path + '/preprocessed_videos/' + folder):
+        os.mkdir(save_base_path + '/preprocessed_videos/' + folder)
+        print("Made " + folder + " directory!")
 
     # Resize video to optimal size
-    file_path = base + '/' + filename
-    save_path = save_base_path + '/preprocessed_videos/' + filename + '.mp4'
+    file_path = base + '/' + folder + '/' + filename
+    save_path = save_base_path + '/preprocessed_videos/' + folder + '/' + filename + '.mp4'
     os.system("ffmpeg -i " + file_path + " -vf scale=720:404 -an " + save_path)
     print("Resized, muted and saved!")
 
 if __name__ == '__main__':
-    base_path_arg = get_base_path_argument()
-    save_base_path = ""
-    if base_path_arg is not None:
-        base_path = base_path_arg
-        save_base_path = base_path_arg
-
     # Get dataset folders
+    # dirs = os.listdir(base_path)
     files = os.listdir(base_path)
 
     # Loop in each folder
+    # for folder in dirs:
+    #     files = os.listdir(base_path + '/' + folder)
     for filename in files:
-        transform_video(base_path, filename, save_base_path)
+        transform_video(base_path, "ASD", filename)
